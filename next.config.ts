498fa2cc7837e8c1dd674251dab9c7881056ef0c
Georgetsn29 <giotsnobiladze5@gmail.com>
2025-11-17 16:20:33 +0400

    zx

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,16 @@ const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
 
-  // 🔑 Conditional Base Path: Only apply the path when building for production/export.
-  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio1' : '', 
+  // basePath: process.env.NODE_ENV === 'production' ? '/Portfolio1' : '', 
+
+  basePath: "/Portfolio",
+
   
-  // Optional/Recommended settings
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  
 };
 
 export default nextConfig;
